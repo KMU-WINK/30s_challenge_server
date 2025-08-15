4b0d2cccd9afcd052c0de5d75c37ffcf79ae59c7
arirang030 <arirang030@kookmin.ac.kr>
2025-08-15 21:29:28 +0900

    Initial commit

---
+++ src/main/java/com/github/thirty_day_challenge/ThirtyDayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.github.thirty_day_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirtyDayChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirtyDayChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/thirty_day_challenge/ThirtyDayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thirty_day_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirtyDayChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
