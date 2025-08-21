7247ff6fa1ee220cc4b9331b03b09884974eda9a
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-21 13:45:24 +0900

    feat(challenge): challenge repository findByUser 추가

--- src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/repository/ChallengeRepository.java
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/repository/ChallengeRepository.java
@@ -3,7 +3,10 @@ package com.github.thirty_day_challenge.domain.user._challenge.repository;
 import com.github.thirty_day_challenge.domain.user._challenge.entity.Challenge;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChallengeRepository extends CrudRepository<Challenge, UUID> {
+
+    List<Challenge> findByUser(User user);
 }
