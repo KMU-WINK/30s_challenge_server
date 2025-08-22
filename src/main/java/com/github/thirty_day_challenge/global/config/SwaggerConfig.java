678f8cbae83a68c2ebdb18391f6da885fffbcde7
arirang030 <arirang030@kookmin.ac.kr>
2025-08-22 17:14:35 +0900

    [CH-04] refactor(swagger): 유저 숨기기

--- src/main/java/com/github/thirty_day_challenge/global/config/SwaggerConfig.java
+++ src/main/java/com/github/thirty_day_challenge/global/config/SwaggerConfig.java
@@ -1,13 +1,22 @@
 package com.github.thirty_day_challenge.global.config;
 
+import com.github.thirty_day_challenge.domain.user.entity.User;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import jakarta.annotation.PostConstruct;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @PostConstruct
+    public void init() {
+
+        SpringDocUtils.getConfig().addRequestWrapperToIgnore(User.class);
+    }
 
     @Bean
     public OpenAPI openAPI() {
