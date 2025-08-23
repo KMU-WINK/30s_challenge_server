c85852f72031ca4cbe465d984e9ce068b6272591
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-23 22:10:05 +0900

    feat(challenge): UserChallenge 엔티티 생성

---
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_user_challenge/entity/UserChallenge.java
@@ -0,0 +1,40 @@
+package com.github.thirty_day_challenge.domain.user._user_challenge.entity;
+
+import com.github.thirty_day_challenge.domain.user._challenge.entity.Challenge;
+import com.github.thirty_day_challenge.domain.user.entity.User;
+import com.github.thirty_day_challenge.global.infra.mysql.BaseSchema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserChallenge extends BaseSchema {
+
+    boolean isOwner;
+
+    boolean hasBadge;
+
+    ParticipantStatus status;
+
+    enum ParticipantStatus {
+        WAITING,
+        ACCEPT,
+        REJECT
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id", nullable = false)
+    Challenge challenge;
+}
