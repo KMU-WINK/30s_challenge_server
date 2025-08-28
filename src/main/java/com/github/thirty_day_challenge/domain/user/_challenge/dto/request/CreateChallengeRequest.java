d1dbed914ec88db81af338ddb41968fff961b471
ahyng <ahyng1@gmail.com>
2025-08-28 22:08:19 +0900

    feat(challenge): CreateChallengeRequest에 제약 어노테이션 추가

--- src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/dto/request/CreateChallengeRequest.java
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/dto/request/CreateChallengeRequest.java
@@ -1,5 +1,7 @@
 package com.github.thirty_day_challenge.domain.user._challenge.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,9 +11,17 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class CreateChallengeRequest {
 
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String description;
+
+    @NotBlank
     private LocalDate startedAt;
+
+    @NotBlank
     private LocalDate endedAt;
+
     private Integer limit;
 }
