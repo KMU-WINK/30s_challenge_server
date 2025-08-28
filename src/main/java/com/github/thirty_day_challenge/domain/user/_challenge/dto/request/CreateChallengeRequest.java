40b4c0ff2787c762e42ab0beb05b4ea92c440dc1
ahyng <ahyng1@gmail.com>
2025-08-28 22:26:49 +0900

    fix(challenge): CreateChallengeRequest 어노테이션 수정

--- src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/dto/request/CreateChallengeRequest.java
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/dto/request/CreateChallengeRequest.java
@@ -1,13 +1,17 @@
 package com.github.thirty_day_challenge.domain.user._challenge.dto.request;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateChallengeRequest {
 
@@ -17,11 +21,15 @@ public class CreateChallengeRequest {
     @NotBlank
     private String description;
 
-    @NotBlank
+    @NotNull
+    @FutureOrPresent
     private LocalDate startedAt;
 
-    @NotBlank
+    @NotNull
+    @FutureOrPresent
     private LocalDate endedAt;
 
+    @NotNull
+    @Positive
     private Integer limit;
 }
