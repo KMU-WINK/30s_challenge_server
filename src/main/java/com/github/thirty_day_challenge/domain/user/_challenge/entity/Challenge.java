d85921e33efa7e182e36ef8abfe6b15d15fbccef
yunnnjeong <yunnnjung0694@naver.com>
2025-08-29 02:25:10 +0900

    feat(challenge): 챌린지 취소 API 구현

---
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/entity/Challenge.java
@@ -0,0 +1,52 @@
+package com.github.thirty_day_challenge.domain.user._challenge.entity;
+
+import com.github.thirty_day_challenge.domain.user._user_challenge.entity.UserChallenge;
+import com.github.thirty_day_challenge.global.infra.mysql.BaseSchema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Challenge extends BaseSchema {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private LocalDate startedAt;
+
+    @Column(nullable = false)
+    private LocalDate endedAt;
+
+    @Column(name = "limit_count")
+    private Integer limit;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private ChallengeStatus status = ChallengeStatus.ACTIVE;
+
+    public enum ChallengeStatus {
+        ACTIVE,
+        CANCEL
+    }
+
+    @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<UserChallenge> userChallenge = new ArrayList<>();
+}
