810fe9d1bfa0df4915ee8e52ff10cb3209f319c5
ahyng <ahyng1@gmail.com>
2025-08-28 20:58:17 +0900

    feat(challenge): 챌린지 개설 API 구현

--- src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/service/ChallengeService.java
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/service/ChallengeService.java
@@ -1,12 +1,54 @@
 package com.github.thirty_day_challenge.domain.user._challenge.service;
 
+import com.github.thirty_day_challenge.domain.user._challenge.dto.request.CreateChallengeRequest;
+import com.github.thirty_day_challenge.domain.user._challenge.dto.response.CreateChallengeResponse;
+import com.github.thirty_day_challenge.domain.user._challenge.entity.Challenge;
 import com.github.thirty_day_challenge.domain.user._challenge.repository.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
+
+    String code = generateUniqueCode();
+
+    public CreateChallengeResponse create(CreateChallengeRequest request) {
+
+        Challenge challenge = Challenge.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .startedAt(request.getStartedAt())
+                .endedAt(request.getEndedAt())
+                .limit(request.getLimit())
+                .code(code)
+                .build();
+
+        Challenge savedChallenge = challengeRepository.save(challenge);
+
+        return new CreateChallengeResponse(
+                savedChallenge.getId(),
+                savedChallenge.getName(),
+                savedChallenge.getDescription(),
+                savedChallenge.getCode(),
+                savedChallenge.getStartedAt(),
+                savedChallenge.getEndedAt(),
+                savedChallenge.getLimit(),
+                savedChallenge.getStatus()
+        );
+    }
+
+    private String generateUniqueCode() {
+
+        String code;
+
+        do {
+            code = UUID.randomUUID().toString().substring(0, 8);
+        } while (challengeRepository.existsByCode(code));
+        return code;
+    }
 }
