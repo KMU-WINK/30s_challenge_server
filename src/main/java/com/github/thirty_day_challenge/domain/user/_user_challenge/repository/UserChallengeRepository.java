8f6e099598ed9e8c8237f271c8a81fc57a66becd
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-29 03:24:51 +0900

    feat(challenge): findChallengesByUser 추가

---
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_user_challenge/repository/UserChallengeRepository.java
@@ -0,0 +1,14 @@
+package com.github.thirty_day_challenge.domain.user._user_challenge.repository;
+
+import com.github.thirty_day_challenge.domain.user._challenge.entity.Challenge;
+import com.github.thirty_day_challenge.domain.user._user_challenge.entity.UserChallenge;
+import com.github.thirty_day_challenge.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserChallengeRepository extends JpaRepository<UserChallenge, UUID> {
+
+    List<Challenge> findChallengesByUser(User user);
+}
