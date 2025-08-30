00f0ce1ed94bb91cabf668acc5fe563167d2a125
arirang030 <arirang030@kookmin.ac.kr>
2025-08-30 18:31:48 +0900

    [CH-08] chore(challenge): rebase

--- src/main/java/com/github/thirty_day_challenge/domain/user/_challenge/repository/ChallengeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.thirty_day_challenge.domain.user._challenge.repository;
-
-import com.github.thirty_day_challenge.domain.user._challenge.entity.Challenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ChallengeRepository extends JpaRepository<Challenge, UUID> {
-}
