86928e76ec1653c41d72011a8e5fcd2b7e54d4b6
yunnnjeong <yunnnjung0694@naver.com>
2025-09-05 22:38:03 +0900

    fix(dailyRecord): 예외 처리 ResponseStatusException(HttpStatus.FORBIDDEN)으로 수정

--- src/main/java/com/github/thirty_day_challenge/domain/user/_daily_record/controller/DailyRecordController.java
+++ src/main/java/com/github/thirty_day_challenge/domain/user/_daily_record/controller/DailyRecordController.java
@@ -9,8 +9,10 @@ import com.github.thirty_day_challenge.domain.user.entity.User;
 import com.github.thirty_day_challenge.global.util.CurrentUser;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -32,7 +34,9 @@ public class DailyRecordController {
     ) {
         UserChallenge userChallenge = userChallengeRepository
                 .findByUserIdAndChallengeId(user.getId(), challengeId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 챌린지에 참여하고 있지 않습니다."));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.FORBIDDEN, ("해당 챌린지에 참여하고 있지 않습니다."))
+                );
 
         return ResponseEntity.ok(dailyRecordService.getDailyRecords(userChallenge));
     }
